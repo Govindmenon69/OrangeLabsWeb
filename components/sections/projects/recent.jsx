d4be5a10816a6268c7ae88f5147a6b45680f4610
Govind Menon <91384825+Govindmenon69@users.noreply.github.com>
2024-11-12 23:11:13 +0530

    hehe

---
+++ components/sections/projects/recent.jsx
@@ -0,0 +1,24 @@
+// Section structure
+import Section 		from '../../structure/section';
+import Container 	from '../../structure/container';
+
+import Image from 'next/image'
+
+import Icon from '../../utils/icon.util'
+
+import css from '../../../styles/sections/projects/recent.module.scss'
+
+export default function GitProjects({ repos, user }) {
+	return (
+		<Section classProp={css.section}>	
+			<Container classProp={css.container} spacing={'verticalXXXLrg'}>
+				<h3>More coming soon...</h3>
+			
+				{/*
+				<pre>{ JSON.stringify(user, undefined, 2) }</pre>
+				<pre>{ JSON.stringify(repos, undefined, 2) }</pre>
+				*/}
+			</Container>
+		</Section>
+	)
+}
